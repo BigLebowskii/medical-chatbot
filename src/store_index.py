064b1926fda62120724200bee24b1cb422073bcf
BigLebowskii <fuatsonbeat141@gmail.com>
2025-03-07 12:59:19 +0500

    fixed bugs, added interactive ui, made few changes

--- src/store_index.py
+++ src/store_index.py
@@ -2,19 +2,19 @@ from src.helper import load_pdf_file, text_split, download_hugging_face_embeddin
 from pinecone.grpc import PineconeGRPC as Pinecone
 from pinecone import ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
-from dotenv import dot_env
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 
-extracted_data=load_pdf_file(data='data/')
+extracted_data=load_pdf_file(data='/home/sami/work/projects/medical-chatbot/data/')
 text_chunks=text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
-index_name = "med-bot"
+index_name = "medical-bot"
 
 pc.create_index(
     name=index_name,
