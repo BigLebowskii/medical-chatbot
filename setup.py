bc907823eb4ab4ce702d50b852ab60dd1d805c6b
BigLebowskii <fuatsonbeat141@gmail.com>
2025-03-07 10:19:41 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'xxx',
+    author_email = 'xxx@xxx.com',
+    packages = find_packages(),
+    install_requires = [])
